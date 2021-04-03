59eb81bb0adf65bf065bd8494923023fea10311a
feten <hajboubaker.feten@gmail.com>
2021-04-03 23:02:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import Crousel from './components/Crousel';
+
+function App() {
+  return (
+    <div>
+   <Navbar />
+   <Crousel /></div>
+   
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import logo from "../components/img/logo.png";
+
+
+const navbar = () => {
+    return (
+   
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      
+      <a className="navbar-brand" href="#"><img src={logo} className="logo"></img></a>
+      <div className="container">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Accueil <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Découvrir</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Evènements
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          
+        </ul>
+        
+      </div>
+      </div>
+    </nav>
+     
+    
+
+
+       
+    )
+}
+
+export default navbar
